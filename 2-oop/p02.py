5813a86c0d56cb13a346880eabb2263d699f80c0
Keyman Li <1047602507@qq.com>
2018-06-06 22:57:54 +0800

    first modify

--- 2-oop/p02.py
+++ 2-oop/p02.py
@@ -1,1 +1,15 @@
 import math
+print(math.ceil(5.001))
+
+print(math.floor(5.001))
+
+import keyword
+print(round(5.45))
+#sqrt是开平方
+print(math.sqrt(4))
+#pow是开平方
+print(math.pow(4,3))
+#幂运算
+print(5**2)
+
+
